76b5ba26e20fdd5a0ac9a22733f1355c89fecf4b
Andre Cruz <andre.cruz@co.sapo.pt>
2015-12-17 12:13:23 +0000

    Wait for services.

---
+++ wait_for_services.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function test_postgresql {
+  pg_isready -h "${DB_HOST}" -U "postgres"
+}
+
+count=0
+# Chain tests together by using &&
+until ( test_postgresql )
+do
+  ((count++))
+  if [ ${count} -gt 50 ]
+  then
+    echo "Services didn't become ready in time"
+    exit 1
+  fi
+  sleep 0.1
+done
